ea5a9f1d4c78e07309f06081755dac6b399511e5
SurajAwatade <114577399+SurajAwatade@users.noreply.github.com>
2025-02-05 00:35:05 +0530

    Updated code: register primary user and secondry user>> Keycloak Admin client

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,66 @@
 # app/auth.py
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import FastAPI, APIRouter, HTTPException
 import requests
 from app.config import KEYCLOAK_BASE, KEYCLOAK_REALM, KEYCLOAK_CLIENT_ID, KEYCLOAK_CLIENT_SECRET, KEYCLOAK_REDIRECT_URI
 
 router = APIRouter()
+
+from keycloak import KeycloakAdmin
+from keycloak import KeycloakOpenIDConnection
+
+keycloak_connection = KeycloakOpenIDConnection(
+                        server_url=KEYCLOAK_BASE,
+                        username='mediscus',
+                        password='mediscus21',
+                        realm_name="master",
+                        # user_realm_name="only_if_other_realm_than_master",
+                        client_id=KEYCLOAK_CLIENT_ID,
+                        client_secret_key=KEYCLOAK_CLIENT_SECRET,
+                        verify=False)
+
+keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
+
+@router.post("/register")
+def register_primary_user(user_data: dict):
+    role = keycloak_admin.get_realm_role("primary_user")
+    """ Register a new primary user """
+    user_id = keycloak_admin.create_user({
+        "username": user_data["username"],
+        "email": user_data["email"],
+        "firstName": user_data["firstName"],
+        "lastName": user_data["lastName"],
+        "enabled": True,
+        # "attributes": {
+        #     "linked_accounts": "[]"
+        # }
+    })
+    print("---------user---------", user_id)
+    keycloak_admin.assign_realm_roles(user_id, role)
+    return {"message": "Primary user registered successfully"}
+
+
+@router.post("/add-family-member")
+def add_family_member(family_member_data: dict):
+    """ Create a family member account linked to the primary user """
+    role = keycloak_admin.get_realm_role("secondary_user")
+    print("role", role)
+    primary_user_id = family_member_data['primary_user_id']
+    print("primary_user_id", primary_user_id)
+    family_member = keycloak_admin.create_user({
+        "username": family_member_data["username"],
+        "email": family_member_data["email"],
+        "firstName": family_member_data["firstName"],
+        "lastName": family_member_data["lastName"],
+        "enabled": True,
+        "attributes": {
+            "primary_account": primary_user_id
+        }
+    })
+    print("family_member", family_member)
+    # Assign secondary role
+    keycloak_admin.assign_realm_roles(family_member, role)
+        
+    return {"message": "Family member added successfully"}
 
 def get_token_url():
     return f"{KEYCLOAK_BASE}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token"
